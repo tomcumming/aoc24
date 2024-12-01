efb5db53c56ba73964141d92d8e5f15827c6b62b
Tom Cumming <mail@tomcumming.co.uk>
2024-12-01 12:03:30 +0000

    Day 1 part 2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,22 +1,30 @@
-pub fn day1(part_two: bool) {
-    assert!(!part_two);
+use std::collections::BTreeMap;
 
+pub fn day1(part_two: bool) {
     let ls = std::io::stdin().lines().map(|l| l.unwrap());
-    println!("{}", day1_part1(ls));
+    let res = if part_two {
+        day1_part2(ls)
+    } else {
+        day1_part1(ls)
+    };
+    println!("{}", res);
+}
+
+fn parse_input(ls: impl Iterator<Item = String>) -> Vec<(i32, i32)> {
+    ls.map(|l| {
+        l.split_whitespace()
+            .map(|s| s.parse().expect("Could not parse input number"))
+            .collect::<Vec<i32>>()
+    })
+    .map(|ns| match ns.as_slice() {
+        [x, y] => (*x, *y),
+        _ => panic!("Wrong number of numbers on line"),
+    })
+    .collect()
 }
 
 fn day1_part1(ls: impl Iterator<Item = String>) -> i32 {
-    let pairs: Vec<(i32, i32)> = ls
-        .map(|l| {
-            l.split_whitespace()
-                .map(|s| s.parse().expect("Could not parse input number"))
-                .collect::<Vec<i32>>()
-        })
-        .map(|ns| match ns.as_slice() {
-            [x, y] => (*x, *y),
-            _ => panic!("Wrong number of numbers on line"),
-        })
-        .collect();
+    let pairs = parse_input(ls);
     let mut lefts: Vec<i32> = pairs.iter().map(|(x, _)| *x).collect();
     lefts.sort();
     let mut rights: Vec<i32> = pairs.iter().map(|(_, y)| *y).collect();
@@ -29,16 +37,44 @@ fn day1_part1(ls: impl Iterator<Item = String>) -> i32 {
         .sum()
 }
 
-#[test]
-fn test_day1_part1() {
-    let input = "3   4
+fn day1_part2(ls: impl Iterator<Item = String>) -> i32 {
+    let pairs = parse_input(ls);
+    let lefts: Vec<i32> = pairs.iter().map(|(x, _)| *x).collect();
+    let rights: BTreeMap<i32, i32> =
+        pairs
+            .iter()
+            .map(|(_, y)| *y)
+            .fold(BTreeMap::new(), |mut p, y| {
+                p.entry(y).and_modify(|c| *c = *c + 1).or_insert(1);
+                p
+            });
+
+    lefts
+        .into_iter()
+        .map(|x| rights.get(&x).unwrap_or(&0) * x)
+        .sum()
+}
+
+#[cfg(test)]
+mod test {
+    fn test_input() -> impl Iterator<Item = String> {
+        "3   4
 4   3
 2   5
 1   3
 3   9
 3   3"
-        .lines()
-        .map(|s| s.to_string());
+            .lines()
+            .map(|s| s.to_string())
+    }
 
-    assert_eq!(day1_part1(input), 11);
+    #[test]
+    fn test_day1_part1() {
+        assert_eq!(super::day1_part1(test_input()), 11);
+    }
+
+    #[test]
+    fn test_day1_part2() {
+        assert_eq!(super::day1_part2(test_input()), 31);
+    }
 }
